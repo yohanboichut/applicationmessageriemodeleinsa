1f20e9b7f3b862f5a25c22bebd8322d4c2603d95
Boichut Yohan <yohan.boichut@univ-orleans.fr>
2020-02-03 22:00:09 +0100

    Service complet

--- src/main/java/rmistuff/MonService.java
+++ src/main/java/rmistuff/MonService.java
@@ -1,10 +1,12 @@
 package rmistuff;
 
-import facade.exceptions.InformationsNonConformesException;
-import facade.exceptions.LoginDejaPrisException;
+import facade.MessageLight;
+import facade.UtilisateurLight;
+import facade.exceptions.*;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Collection;
 
 /**
  * Created by YohanBoichut on 08/10/14.
@@ -16,6 +18,76 @@ public interface MonService extends Remote{
 
 
     void inscription(String login, String password) throws LoginDejaPrisException, InformationsNonConformesException, RemoteException;
+
+
+
+    /**
+     * Permet de se désabonner de l'application
+     * @param login
+     * @param password
+     * @throws DonneesIncoherentesException : le couple login/password n'est pas correct
+     * @throws InformationsNonConformesException : Au moins une des informations n'est pas conforme (vide ou non
+     * renseignée
+     */
+    void resiliser(String login, String password) throws DonneesIncoherentesException, InformationsNonConformesException, RemoteException;
+
+
+
+    /**
+     * Permet de se connecter à l'application
+     * @param login
+     * @param password
+     * @throws DonneesIncoherentesException : le couple login/password n'est pas correct
+     * @throws InformationsNonConformesException : Au moins une des informations n'est pas conforme (vide ou non
+     * renseignée
+     */
+    long connexion(String login, String password)throws LoginDejaPrisException, InformationsNonConformesException, UtilisateurDejaConnecteException, RemoteException;
+
+
+    /**
+     * Permet de se déconnecter de l'application
+     * @param idUtilisateur : identifiant de l'utilisateur demandant la déco
+     * @throws InformationsNonConformesException
+     */
+
+    void deconnexion(long idUtilisateur) throws InformationsNonConformesException, RemoteException;
+
+
+    /**
+     * Permet d'envoyer un nouveau message de idEnvoyeur -> idDestinataire
+     * @param idEnvoyeur
+     * @param idDestinataire
+     * @param contenu
+     * @return id du message envoyé
+     */
+
+    long envoyerUnMessage(long idEnvoyeur, long idDestinataire, String contenu) throws UtilisateurNonConnecteException, UtilisateurInexistantException, RemoteException;
+
+
+    /**
+     * Permet de récupérer la liste des messages de l'utilisateur idUtilisateur. Cet utilisateur doit être connecté
+     * @param idUtilisateur
+     * @return
+     * @throws UtilisateurNonConnecteException : l'utilisateur spécifié n'est pas connecté
+     */
+
+    Collection<MessageLight> getMesMessages(long idUtilisateur) throws UtilisateurNonConnecteException, RemoteException;
+
+
+    /**
+     * Permet de récupérer la liste des personnes inscrites à l'application. L'utilisateur idUtilisateur doit être connecté
+     * @param idUtilisateur
+     * @return
+     * @throws UtilisateurNonConnecteException : l'utilisateur spécifié n'est pas connecté
+     */
+
+    Collection<UtilisateurLight> getListeDesInscrits(long idUtilisateur) throws UtilisateurNonConnecteException, UtilisateurInexistantException, RemoteException;
+
+
+
+    MessageLight getMessageById(long idUtilisateur, long idMessage) throws UtilisateurNonConnecteException,
+            MessageInexistantException, UtilisateurInexistantException, RemoteException;
+
 
 
 }
--- src/main/java/rmistuff/MonServiceImpl.java
+++ src/main/java/rmistuff/MonServiceImpl.java
@@ -2,10 +2,12 @@ package rmistuff;
 
 import facade.FacadeApplicationMessagerie;
 import facade.FacadeApplicationMessagerieImpl;
-import facade.exceptions.InformationsNonConformesException;
-import facade.exceptions.LoginDejaPrisException;
+import facade.MessageLight;
+import facade.UtilisateurLight;
+import facade.exceptions.*;
 
 import java.rmi.RemoteException;
+import java.util.Collection;
 
 /**
  * Created by YohanBoichut on 08/10/14.
@@ -18,5 +20,40 @@ public class MonServiceImpl implements MonService {
     @Override
     public void inscription(String login, String password) throws LoginDejaPrisException, InformationsNonConformesException, RemoteException {
         facadeApplicationMessagerie.inscription(login,password);
+    }
+
+    @Override
+    public void resiliser(String login, String password) throws DonneesIncoherentesException, InformationsNonConformesException, RemoteException {
+        facadeApplicationMessagerie.resiliser(login, password);
+    }
+
+    @Override
+    public long connexion(String login, String password) throws LoginDejaPrisException, InformationsNonConformesException, UtilisateurDejaConnecteException, RemoteException {
+        return facadeApplicationMessagerie.connexion(login, password);
+    }
+
+    @Override
+    public void deconnexion(long idUtilisateur) throws InformationsNonConformesException, RemoteException {
+        facadeApplicationMessagerie.deconnexion(idUtilisateur);
+    }
+
+    @Override
+    public long envoyerUnMessage(long idEnvoyeur, long idDestinataire, String contenu) throws UtilisateurNonConnecteException, UtilisateurInexistantException, RemoteException {
+        return facadeApplicationMessagerie.envoyerUnMessage(idEnvoyeur, idDestinataire, contenu);
+    }
+
+    @Override
+    public Collection<MessageLight> getMesMessages(long idUtilisateur) throws UtilisateurNonConnecteException, RemoteException {
+        return facadeApplicationMessagerie.getMesMessages(idUtilisateur);
+    }
+
+    @Override
+    public Collection<UtilisateurLight> getListeDesInscrits(long idUtilisateur) throws UtilisateurNonConnecteException, UtilisateurInexistantException, RemoteException {
+        return facadeApplicationMessagerie.getListeDesInscrits(idUtilisateur);
+    }
+
+    @Override
+    public MessageLight getMessageById(long idUtilisateur, long idMessage) throws UtilisateurNonConnecteException, MessageInexistantException, UtilisateurInexistantException, RemoteException {
+        return getMessageById(idUtilisateur, idMessage);
     }
 }
